b8329e839bc883c798756d0f2caa9092e0f01735
Patrick Edet <grandpato001@gmail.com>
2023-03-17 16:50:32 +0100

    Social Iconsreplaced

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Interests(){
     return(<div className='Interests'>
              
-        <h3>Interests</h3>
+        <h3 className="Interest_title">Interests</h3>
         <p>
             A Techie. Chess Player and Trainer (FIDE Rated).Public Speaker. Sign Language Interpreter(Certified).
         </p>
